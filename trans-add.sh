4df4ad82acf87c187b0497e5b4c0dc88f7f3d918
Binbin Qian <qianbinbin@hotmail.com>
2021-08-27 21:05:04 +0800

    Update

--- trans-add.sh
+++ trans-add.sh
@@ -3,20 +3,18 @@
 MAGNETS_FILE="$HOME/.config/transmission-daemon/trans-add.magnet"
 [ -n "$1" ] && MAGNETS_FILE="$1"
 
-error() {
-  echo "$@" >&2
-}
+error() { echo "$@" >&2; }
 
 log() {
-  echo "[$(date +%F-%T)]" "$@"
+  while read -r REPLY; do
+    error "[$(date +%F-%T)] $REPLY"
+  done
 }
 
-exist() {
-  which "$1" >/dev/null 2>&1
-}
+exist() { which "$1" >/dev/null 2>&1; }
 
-exist curl || (error "curl not found" && exit 127)
-exist transmission-remote || (error "transmission-remote not found" && exit 127)
+exist curl || { error "curl not found"; exit 127; }
+exist transmission-remote || { error "transmission-remote not found"; exit 127; }
 
 URLS=$(cat)
 
@@ -33,10 +31,10 @@ for url in $URLS; do
     # magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a
     urn=$(echo "$magnet" | grep -i "=urn:.*:[^&]*" -o | cut -c2-)
     if grep -qs "$urn" "$MAGNETS_FILE"; then
-      log "Skipping: $urn"
+      echo "Skipping: $urn" | log
       continue
     fi
-    log "Adding: $urn"
+    echo "Adding: $urn" | log
     transmission-remote -a "$magnet" && echo "$magnet" >>"$MAGNETS_FILE"
   done
 done
